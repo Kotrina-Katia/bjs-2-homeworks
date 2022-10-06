7a1abb5fcd6581b6099ae17f0de1815d5c9ea267
Kotrina-Katia <skifinia@gmail.com>
2022-10-06 11:52:30 +0300

    second commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,13 +4,13 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = (b**2) - (4*a*c);
+  const descriminant = (b**2) - (4*a*c);
 
-  if (d === 0) {
+  if (descriminant === 0) {
     arr[0] = -b/(2*a);
-  } else if (d > 0) {
-    arr[0] = (-b + Math.sqrt(d) )/(2*a);
-    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  } else if (descriminant > 0) {
+    arr[0] = (-b + Math.sqrt(descriminant) )/(2*a);
+    arr[1] = (-b - Math.sqrt(descriminant) )/(2*a);
   }
   return arr;
 }
@@ -20,9 +20,22 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-  let S = amount - contribution;
-  let n = (mm - mm0) + ((dd - dd0)*12/365) + ((yyyy - yyyy0)*12);
-  let payment = S*(P + (P/((1 + P)**n) - 1));
-  totalAmount = payment * n;
+
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+    let loanBody = amount - contribution;
+    let S = loanBody;
+    let numberOfMonths = (date.getFullYear() - new Date().getFullYear())*12 + date.getMonth() - new Date().getMonth();
+    let n = numberOfMonths;
+    let P = percent/(12*100);
+    let payment = S*(P + (P/(((1 + P)**n) - 1)));
+    totalAmount = payment*n;
+    totalAmount = +totalAmount.toFixed(2);
+  }
   return totalAmount;
 }

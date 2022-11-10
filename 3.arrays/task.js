3f329f80009c7e6d28b167a817a0ea8fc9867361
Kotrina-Katia <skifinia@gmail.com>
2022-11-10 08:43:28 +0200

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
+// Задача №1. Сравнить массивы
+
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if(arr1.length === arr2.length) {
+    for(i = 0; i < arr1.length; i++) {
+      if(arr1[i] === arr2[i]) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
   return result; // boolean
 }
 
+// Задача №2. Фильтрация и преобразование массива
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 ===0).map(number => number * 10);
   return resultArr; // array
 }

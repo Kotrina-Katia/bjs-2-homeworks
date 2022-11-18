43353b27049e3eabd6a92d86aa572f1c1a9ed855
Kotrina-Katia <skifinia@gmail.com>
2022-11-18 11:20:33 +0200

    second variant

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,24 +1,14 @@
 // Задача №1. Сравнить массивы
 
 function compareArrays(arr1, arr2) {
-  let result;
-  if(arr1.length === arr2.length) {
-    for(i = 0; i < arr1.length; i++) {
-      if(arr1[i] === arr2[i]) {
-        result = true;
-      } else {
-        result = false;
-      }
-    }
-  } else {
-    result = false;
-  }
+  let result = arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i]);
   return result; // boolean
 }
+
 
 // Задача №2. Фильтрация и преобразование массива
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 ===0).map(number => number * 10);
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
   return resultArr; // array
 }

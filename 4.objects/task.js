868ebcb95c2d78e1b8d45c9fa702df212b844da3
Kotrina-Katia <skifinia@gmail.com>
2022-11-22 21:45:03 +0200

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
+// Задача №1. Инкапсуляция студента
+
+// Пункт 1
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let stud1 = new Student("Gleb", "male", 32);
+let stud2 = new Student("Olga", "female", 42);
+let stud3 = new Student("Lev", "male", 22);
+
+// Пункт 2
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+// Пункт 3
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+// Пункт 4
+Student.prototype.addMarks = function(...mark1) {
+  if(this.marks === undefined) {
+    this.marks = [...mark1];
+  } else {
+    this.marks.push([...mark1]);
+  }
+}
+
+// Пункт 5
+Student.prototype.getAverage = function(addMark, addMarks) {
+  this.average = Math.sum(...addMark, ...addMarks) / (addMark.length + addMarks.length);
+}
+
+// Пункт 6
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
